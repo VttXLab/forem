1eb482e6f206ca16972b50a83edd876038128b7e
Tran Thang Long <longtranthang@gmail.com>
2023-01-14 10:49:14 +0700

    Comments: Add NSFW detection feature

--- app/assets/javascripts/initializers/initQuickshare.js
+++ app/assets/javascripts/initializers/initQuickshare.js
@@ -8,3 +8,42 @@ function QSFBEmbedParse(timeout = 2000) {
         }
     }, timeout);
 }
+
+function nsfwImageFixLink() {
+    var nsfwImages = document.querySelectorAll('.nsfw-content');
+    for (let nsfwImage of nsfwImages) {
+        let a = nsfwImage.parentElement;
+        
+        if (a.tagName != 'A') {
+            continue
+        }
+
+        if (a.dataset.href) {
+            continue
+        }
+
+        a.dataset.href = a.href;
+        a.setAttribute("href","javascript:void(0);");
+    }
+}
+
+function nsfwImageClick(e) {
+    let target = e.target;
+    let parent = e.target.parentElement;
+
+    let isNsfw = target.classList.contains('nsfw-content');
+    if (!isNsfw) {
+        return
+    }
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    target.classList.remove('nsfw-content');
+    parent.setAttribute("href", parent.dataset.href);
+}
+
+function nsfwParse() {
+    nsfwImageFixLink();
+    document.addEventListener('click', nsfwImageClick);
+}
